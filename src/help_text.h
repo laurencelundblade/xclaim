22a2b74640303dc8ef98907257a6bdeec69bf705
Laurence Lundblade <lgl@securitytheory.com>
2021-03-12 17:47:49 -0800

    -help option is working

---
+++ src/help_text.h
@@ -0,0 +1,14 @@
+//
+//  help_text.h
+//  xclaim
+//
+//  Created by Laurence Lundblade on 3/12/21.
+//  Copyright © 2021 Laurence Lundblade. All rights reserved.
+//
+
+#ifndef help_text_h
+#define help_text_h
+
+extern const char *help_text;
+
+#endif /* help_text_h */
